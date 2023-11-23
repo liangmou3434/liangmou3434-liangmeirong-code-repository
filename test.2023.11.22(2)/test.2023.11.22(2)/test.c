c09b417d37f59815bf71db5039f5cf14504024ce
liangmou3434 <2685463636@qq.com>
2023-11-23 08:48:39 +0800

    测试代码包括布尔类型和字符串的计算

---
+++ test.2023.11.22(2)/test.2023.11.22(2)/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+
+
+
+//int main()
+//{
+//	int a = 10;
+//	short s = 4;
+//	printf("%d\n",sizeof(s = a + 5));//sizeof里的表达式不会参与真实的计算-s为短整型最后长度为短整型长度两个字节
+//	printf("%d\n", s);
+//	return 0;
+//}
+
+
+//int main()-布尔类型
+//{
+//	bool flag = true;
+//	if (flag)
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
