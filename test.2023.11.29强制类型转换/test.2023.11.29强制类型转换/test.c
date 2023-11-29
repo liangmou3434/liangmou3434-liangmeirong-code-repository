24ffd66e1b6756dc73be4a73211726962307c168
liangmou3434 <2685463636@qq.com>
2023-11-29 10:22:41 +0800

    强制转换类型

---
+++ test.2023.11.29强制类型转换/test.2023.11.29强制类型转换/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNIGNS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int a;
+//	float f;
+//	a = 20;
+//	f = 24.56;
+//	printf("(float)a=%f\n", (float)a);//将a int类型强制转换成float类型
+//	printf("(int)f=%d\n", (int)f);//将f float类型强制转换成int类型
+//	printf("a=%d,f=%f\n", a, f);
+//	return 0;
+//}
