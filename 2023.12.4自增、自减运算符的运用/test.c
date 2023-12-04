d0f0531d96365889493910a584b57ac1cb870a95
liangmou3434 <2685463636@qq.com>
2023-12-04 15:54:38 +0800

    自增，自减的运算符的使用

---
+++ 2023.12.4自增、自减运算符的运用/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i, k, a, b, c;
+//	i = 3;
+//	k = ++i;//先自增1再赋值给k
+//	printf("i=%d,k=%d\n", i, k);//i=4,k=4
+//	i = 3;
+//	k = i++;//先使用i值赋给k再自增1
+//	printf("i=%d,k=%d\n", i, k);//i=4,k=3
+//	i = 3;
+//	printf("%d\n", ++i);//先自增1再使用i值
+//	i = 3;
+//	printf("%d\n", i++);//先试用i值再自增1
+//	a = 3;
+//	b = 5;
+//	c = (++a) * b;//先自增再使用a值  
+//	printf("a=%d,b=%d,c=%d\n", a, b, c);//a=4,b=5,c=20
+//	c = (a++) * b;//先使用a值再自增,前面一条代码将a自增成4
+//	printf("a=%d,b=%d,c=%d\n", a, b, c);//a=5,b=5,c=20
+//	return 0;
+//}
