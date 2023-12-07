d60b6f4ea59cb559a61e66366fcbd4316c3ca4cd
liangmou3434 <2685463636@qq.com>
2023-12-07 22:59:34 +0800

    比特训练营运算符和循环语句作业

---
+++ 2023.12.7test/test.c
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d%d", &a, &b);
+//    if (a > b)
+//    {
+//        printf("a>b");
+//    }
+//    else
+//    {
+//        printf("a<b");
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    while (scanf("%d%d",&a,&b) != EOF)
+//    {
+//        if (a > b)
+//        {
+//            printf("%d>%d", a, b);
+//        }
+//        else if (a < b)
+//        {
+//            printf("%d<%d", a, b);
+//        }
+//        else if (a == b)
+//        {
+//            printf("%d=%d", a, b);
+//        }
+//    }
+//    return 0;
+//}
+//
+//int main()
+//{
+//    int t = 0;
+//    while (scanf("%d", &t) != EOF)
+//    {
+//        if (t > 0)
+//        {
+//            printf("1\n");
+//        }
+//        else if (t == 0)
+//        {
+//            float a = 1 / 2.0;
+//            printf("%.1f\n", a);
+//        }
+//        else if (t < 0)
+//        {
+//            printf("0\n");
+//        }
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int M = 0;
+//    scanf("%d", &M);
+//    if (M % 5 == 0)
+//    {
+//        printf("YES");
+//    }
+//    else {
+//        printf("NO");
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int length = 0;
+//    int n = 0;
+//    while (scanf(" %d", &n) != EOF)
+//    {
+//        for (length = 1; length <= n; length++)
+//        {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
