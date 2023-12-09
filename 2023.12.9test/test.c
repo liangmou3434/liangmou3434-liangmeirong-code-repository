2f45d2b82e2d052644755f55d190e03cb9020b15
liangmou3434 <2685463636@qq.com>
2023-12-09 22:38:01 +0800

    比特作业if语句部分测试代码

---
+++ 2023.12.9test/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	 int i = 0  //打印1~100中三的倍数
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{                //倒打印数字
+//	int i = 0;
+//	int n = 0;
+//	scanf("%d", &i);
+//	while (i > 0)
+//	{
+//		n = i % 10;
+//		i = i / 10;
+//		printf("%d  ", n);
+//	}
+//	return 0;
+//}
