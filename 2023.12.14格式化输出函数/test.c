15a81d80eeba690f40a46a3ff95e6d2416d97532
liangmou3434 <2685463636@qq.com>
2023-12-14 22:07:36 +0800

    格式化输出各占位符输出

---
+++ 2023.12.14格式化输出函数/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int a = 56;
+//	int b = 255;
+//	int c = 65;
+//	int d = 56;
+//	char ch = 66;
+//	float f1 = 123.456;
+//	float f2 = 123.456;
+//	float f3 = 123.456;
+//	printf("%d\n", a);//输出十进制数字
+//	printf("%x\n", b);//输出十六进制数字
+//	printf("%o\n", c);//输出八进制数字
+//	printf("%u\n", d);//输入十进制数字
+//	printf("%c\n", ch);//输入ch数值对印的十进制数字代表的ASCII码值对应的符号
+//	printf("%s\n", "hello");//输出字符串
+//	printf("%e\n", f1);//输出指数形式浮点数
+//	printf("%f\n", f2);//输出float类型数据
+//	printf("%g\n", f3);//输出指数形式浮点数
+//	printf("%%");//输出百分号
+//	return 0;
+//}
