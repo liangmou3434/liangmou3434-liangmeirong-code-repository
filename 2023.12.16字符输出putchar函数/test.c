bb8e3c28cbdc1be293adbb7a27b4398a4076ddc7
liangmou3434 <2685463636@qq.com>
2023-12-16 23:00:58 +0800

    字符输入函数putchar

---
+++ 2023.12.16字符输出putchar函数/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	char a, b, c;
+//	a = 'N';
+//	b = 'I';
+//	c = 'U';
+//	putchar(a);
+//	purchar(b);
+//	putchar(c);//×Ö·ûÊäÈëº¯Êı
+//	putchar('\n');
+//	putchar(a);
+//	putchar('\n');
+//	putchar(b);
+//	putchar('\n');
+//	putchar(c);
+//	putchar('\n');
+//	return 0;
+//}
