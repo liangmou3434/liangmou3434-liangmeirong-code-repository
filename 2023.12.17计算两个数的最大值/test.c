84e4affb324cc69f521ded04ba7c4f9ac51d7be2
liangmou3434 <2685463636@qq.com>
2023-12-17 20:54:41 +0800

    计算两个数的最大值，简单运用if 语句

---
+++ 2023.12.17计算两个数的最大值/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int a, b, max = 0;
+//	printf("输入a和b的值，用逗号分隔：");
+//	scanf("%d,%d", &a, &b);
+//	if(a >= b)
+//	{
+//		max = a;
+//	}
+//	if (a < b)
+//	{
+//		max = b;
+//	}
+//	printf("最大值为：%d\n", max);
+//	return 0;
+//}
