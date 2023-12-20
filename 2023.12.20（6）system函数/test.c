9e56a4011095ac0e126e3c5a871c823d60051666
liangmou3434 <2685463636@qq.com>
2023-12-20 17:37:34 +0800

    system函数简单使用

---
+++ 2023.12.20（6）system函数/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("第%d个学生的成绩是:", i + 1);
+//		scanf("%d", &arr[i]);
+//		system("cls");//清理屏幕上的信息
+//	}
+//	return 0;
+//}
