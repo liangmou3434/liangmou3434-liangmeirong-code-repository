334d5b38a455a9f3dbc2ccecd0083a9e40f0a25f
liangmou3434 <2685463636@qq.com>
2023-12-20 17:33:20 +0800

    sleep函数简单使用

---
+++ 2023.12.20（5）Sleep函数/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = {0};
+//	for (i=0; i < 10; i++)
+//	{
+//		printf("第%d个学生的成绩是:",i+1);
+//		scanf("%d", &arr[i]);
+//		Sleep(1000);//延迟打印的时间 单位为毫秒-1000毫秒=1秒
+//	}
+//	return 0;
+//}
