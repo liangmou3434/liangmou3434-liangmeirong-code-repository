72b7ed6349882f48c835e97daebb157f025da6a9
liangmou3434 <2685463636@qq.com>
2023-12-27 16:23:36 +0800

    printf函数的链式访问

---
+++ 2023.12.27printf的返回值/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{      打印结果4321-printf库函数的返回值为打印在屏幕上的字符个数
+//	printf("%d ", printf("%d", printf("%d", 43)));
+//	return 0;
+//}
+
+//printf("%d", 43)-打印43两个字符-返回值为2
+// printf("%d", printf("%d", 43))--打印2一个字符返回值为1
+//printf("%d ", printf("%d", printf("%d", 43)));//打印返回值1
