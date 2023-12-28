f0c1b8a1c1b656282ceb734f174096178accf4bb
liangmou3434 <2685463636@qq.com>
2023-12-28 22:28:55 +0800

    数组测试一组

---
+++ 2023.12.28数组作业测试（比特）/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int arr[] = { 1,2,(3,4),5 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("%zd", sizeof(arr));
+//	return 0;
+//}
