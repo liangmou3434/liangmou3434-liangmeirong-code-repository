5632e3de9be272bdab9c0aa9d0ba2ba8bc0d34b4
liangmou3434 <2685463636@qq.com>
2024-01-04 13:44:09 +0800

    最大公约数输出代码

---
+++ 20241.4最大公约数的输出/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	int number1 = 0;
+//	int number2 = 0;
+//	int result = 0;
+//	scanf("%d%d", &number1, &number2);
+//	if (number1 > number2)
+//	{
+//		for (i = 1; i<=number1; i++)
+//		{
+//			if ((number1 / i != 0 && number1 % i == 0) && (number2 / i != 0 && number2 % i == 0))
+//			{
+//				while (i > result)
+//				{
+//					result = i;
+//				}
+//			}
+//		}
+//		printf("%d ", result);
+//	}
+//	else if (number1 < number2)
+//	{         
+//		for (i = 1; i <=number2; i++)
+//		{
+//			if ((number1 / i != 0 && number1 % i == 0) && (number2 / i != 0 && number2 % i == 0))
+//			{
+//				while (i > result)
+//				{
+//					result = i;
+//				}
+//			}
+//		}
+//		printf("%d ", result);
+//	}
+//	return 0;
+//}
