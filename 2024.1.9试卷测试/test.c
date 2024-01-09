4273bcacde93e641900c1feaaf88aa36b40287a7
liangmou3434 <2685463636@qq.com>
2024-01-09 20:55:18 +0800

    简单测试

---
+++ 2024.1.9试卷测试/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//int main()
+//{
+//	char arr1[] = "\\TOP\t\65\"";
+//	int ret = strlen(arr1);
+//	printf("%d ", ret);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	float x = 0;
+//	scanf("%5f", &x);
+//	printf("%f ", x);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	a = 2 * 6, a * 3, a + 5;//运算符优先级问题，乘号优先级大于逗号表达式
+//	printf("%d ", a);
+//	return 0;
+//}
+
