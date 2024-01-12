64a2945541c1495280d1eb8999984f81ce47d0ff
liangmou3434 <2685463636@qq.com>
2024-01-12 19:54:06 +0800

    分段函数的计算

---
+++ 2024.1.12分段函数/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+//int main()
+//{
+//	int x = 0;
+//	while (scanf("%d", &x)!=EOF)
+//	{
+//		if (x >= 0 && x <= 1)
+//		{
+//			printf("该方程的解为：%d", x);
+//		}
+//		else if (x > 1 && x <= 2)
+//		{
+//			printf("这个方程的解为：%lf", sqrt(x * x - 2 * x + 2));
+//		}
+//		else if (x > 2 && x <= 3)
+//		{
+//			printf("这个方程的解为：%lf", sqrt(x * x - 6 * x + 10));
+//		}
+//		else if (x > 3 && x <= 4)
+//		{
+//			printf("这个方程的解为：%d", 4 - x);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
