dfcaf2da3563adb1f0faf22c241939bdf2826385
liangmou3434 <2685463636@qq.com>
2024-01-14 23:57:05 +0800

    求数列的和

---
+++ 2024.1.14求数列的和/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//		int a, b, t, i, n;
+//		double sum = 0.0;
+//		printf("«Î ‰»În£∫");
+//		scanf("%d", &n);
+//		a = 2, b = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			sum = sum + (double)a / b;
+//			t = a;  a = a + b;  b = t;
+//		}
+//		printf("sum=%f\n", sum);
+//	return 0;
+//}
