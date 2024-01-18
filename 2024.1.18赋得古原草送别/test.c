17aa555c35eada9893da1ec46b153153d38bc457
liangmou3434 <2685463636@qq.com>
2024-01-18 21:28:49 +0800

    赋得古原草送别

---
+++ 2024.1.18赋得古原草送别/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	switch (i)
+//	{
+//	case 1:
+//		printf("离离原上草，一岁一枯荣。");
+//		break;
+//	case 2:
+//		printf("野火烧不尽，春风吹又生。");
+//		break;
+//	case 3:
+//		printf("远芳侵古道，晴翠接荒城。");
+//		break;
+//	case 4:
+//		printf("野火烧不尽，春风吹又生。");
+//	default:
+//		printf("您输入了错误的数字。");
+//		break;
+//	}
+//	return 0;
+//}
