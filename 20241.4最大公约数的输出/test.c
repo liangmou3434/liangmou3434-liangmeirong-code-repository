34eb35904ef521583dc5e6a718f2b415aed29481
liangmou3434 <2685463636@qq.com>
2024-03-05 19:19:36 +0800

    test

--- 20241.4最大公约数的输出/test.c
+++ 20241.4最大公约数的输出/test.c
@@ -7,26 +7,26 @@
 //	int number1 = 0;
 //	int number2 = 0;
 //	int result = 0;
-//	scanf("%d%d", &number1, &number2);
+//	scanf("%d%d", &number1, &number2);//输入number1和number2
 //	if (number1 > number2)
 //	{
 //		for (i = 1; i<=number1; i++)
 //		{
-//			if ((number1 / i != 0 && number1 % i == 0) && (number2 / i != 0 && number2 % i == 0))
+//			if ((number1 / i != 0 && number1 % i == 0) && (number2 / i != 0 && number2 % i == 0))//number1和number都能被整除并且！=0
 //			{
 //				while (i > result)
 //				{
-//					result = i;
-//				}
+//					result = i;//把i赋值给result
+//				}    
 //			}
 //		}
-//		printf("%d ", result);
+//		printf("%d ", result);//输出number1和number2能被整除的最大数
 //	}
 //	else if (number1 < number2)
 //	{         
 //		for (i = 1; i <=number2; i++)
 //		{
-//			if ((number1 / i != 0 && number1 % i == 0) && (number2 / i != 0 && number2 % i == 0))
+//			if ((number1 / i != 0 && number1 % i == 0) && (number2 / i != 0 && number2 % i == 0))//number1和number都能被整除并且！=0
 //			{
 //				while (i > result)
 //				{
@@ -34,7 +34,7 @@
 //				}
 //			}
 //		}
-//		printf("%d ", result);
+//		printf("%d ", result);//输出number1和number2能被整除的最大数
 //	}
 //	return 0;
 //}
