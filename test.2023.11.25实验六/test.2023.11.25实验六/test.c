00a47a62c052660bb2d38e1892447d22bfd601ac
liangmou3434 <2685463636@qq.com>
2024-03-05 19:02:37 +0800

    test

--- test.2023.11.25实验六/test.2023.11.25实验六/test.c
+++ test.2023.11.25实验六/test.2023.11.25实验六/test.c
@@ -4,24 +4,24 @@
 //{
 //	double result = 1.0;
 //	int n = 1;
-//	while (1)
+//	while (1)//多次输出阶乘
 //	{
 //		printf("请输入求阶乘中的n（1~100),输入负数退出程序：");
-//		scanf("%d", &n);
+//		scanf("%d", &n);//输入n的值
 //		if (n < 0)
 //		{
-//			break;
+//			break;//如果n<0则循环结束
 //		}
 //		else if (n == 0)
 //		{
-//			continue;
+//			continue;//如果n=0，继续后面的代码
 //		}
 //		else if ((n >= 1) && (n <= 100))
 //		{
 //			 int result = 1,i;
 //			for (i = 1; i <= n; ++i)
 //			{
-//				result *= i;
+//				result *= i;//计算n的阶乘
 //			}
 //			printf("%d的阶乘是%d\n", n, result);
 //		}
@@ -43,7 +43,7 @@
 //	int lowerNum = 0;
 //	int digitNum = 0;
 //	printf("请输入一串字符，按回车结束：");
-//	ch = getchar();//与后面的循环相对应
+//	ch = getchar();//写一次接受一个字符，阅读字符串,接收字符串的第一个字符进入循环
 //	while (ch != '\n')
 //	{
 //		if ((ch >= 'A') && (ch <= 'Z'))
@@ -62,7 +62,7 @@
 //		{
 //			printf("输入不合法\n");
 //		}
-//		ch = getchar();//放在循环里用来遍历字符串
+//		ch = getchar();//在循环内再用一次接收字符
 //	}
 //
 //	printf("大写字母个数：%d\n", upperNum);
@@ -77,7 +77,7 @@
 //	double result = 1.0;
 //	int n = 1;
 //	int i = 1;
-//	printf("请输入求阶乘的n(1~100):");
+//	printf("请输入求阶乘的n(1~100):");//用于计算阶乘
 //	scanf("%d", &n);
 //	if ((n >= 1) && (n <= 100))
 //	{
