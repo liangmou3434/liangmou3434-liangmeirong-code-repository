819b5cf119eb44b035c1995ffdde8a9892212f6d
liangmou3434 <2685463636@qq.com>
2024-03-05 19:13:47 +0800

    test

--- test.11.25/test.11.25/test.c
+++ test.11.25/test.11.25/test.c
@@ -1,13 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<stdlib.h>
 #include<stdbool.h>
-int a = 100;//全局变量
-int main()
-{
-	int a = 10;
-	printf("a=%d", a);//局部变量
-	return 0;
-}
+//int a = 100;//全局变量
+//int main()
+//{
+//	int a = 10;
+//	printf("a=%d", a);//局部变量--优先展示局部变量
+//	return 0;
+//}
 
 //int b;
 //int main()
@@ -17,21 +18,14 @@ int main()
 //	return 0;
 //}
 
-
 //int main()
 //{
-//	bool num = true;
 //	//可以用true or false(num=()也可以换成0或者非0）
 //	//但true和false的可读性更强
+//  bool num = true;
 //	if (num)
 //	{
 //		printf("haha");
 //	}
 //	return 0;
 //}
-//
-
-
-
-
-
