dc434fc4a10794f69b2b396ffb1595f543f9cea6
liangmou3434 <2685463636@qq.com>
2024-03-05 08:28:23 +0800

    test

--- test.2023.11.29实验七/fun.c
+++ test.2023.11.29实验七/fun.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-//char getGradelevel(int grade)
+//char getGradelevel(int grade)//创建函数将学生的成绩分等级
 //{
 //	
 //	if (grade>=0&&grade<60)
@@ -35,20 +35,20 @@
 //	int grade = -1;
 //	char gradeLevel[] = { '0' };
 //	printf("请输入成绩（0~100）\n");
-//	scanf("%d", &grade);
-//	printf("成绩%d的等级是%c\n", grade, getGradelevel(grade));
+//	scanf("%d", &grade);//输入学生的成绩
+//	printf("成绩%d的等级是%c\n", grade, getGradelevel(grade));//调用函数判断学生的成绩等级
 //	printf("So Easy!!!");
 //	return 0;
 //}
 
 
-//float getRectArea(float length, float width)
+//float getRectArea(float length, float width)//创建函数计算矩形的面积
 //{
 //	float num1 = 0;
 //	num1 = length * width;
 //	return num1;
 //}
-//float getCuboiVolume(float length, float width, float height)
+//float getCuboiVolume(float length, float width, float height)//创建函数计算长方体的体积
 //{
 //	float num2 = length * width * height;
 //	return num2;
@@ -61,9 +61,9 @@
 //	float area = 0;
 //	float volume = 0;
 //	printf("请依次输入长度、宽度和高度（如1.0,2.0,3.0):");
-//	scanf("%f,%f,%f", &length, &width, &height);
-//	printf("矩形(长：%f,宽：%f)的面积为:%f\n", length, width, getRectArea( length, width));
-//	printf("矩形（长：%f,宽：%f,高：%f)的体积为：%f\n", length, width, height, getCuboiVolume(length, width, height));
+//	scanf("%f,%f,%f", &length, &width, &height);//输入长方体的长宽高
+//	printf("矩形(长：%f,宽：%f)的面积为:%f\n", length, width, getRectArea( length, width));//调用函数计算矩形的面积
+//	printf("矩形（长：%f,宽：%f,高：%f)的体积为：%f\n", length, width, height, getCuboiVolume(length, width, height));//调用函数计算长方体的体积
 //	return 0;
 //}
 
@@ -74,7 +74,7 @@
 //
 //	for (int i=1;i<=n; i++)
 //	{
-//		result *= i;
+//		result *= i;//创建函数使用for循环计算阶乘
 //	}
 //	return result;
 //}
@@ -83,8 +83,8 @@
 //	double result = 1;
 //	int n = 1;
 //	printf("请输入求阶乘中的n(1~100):");
-//	scanf("%d", &n);
-//	result = fun(n);
+//	scanf("%d", &n);//输入n
+//	result = fun(n);//调用函数计算阶乘
 //	printf("%d的阶乘为%.0lf\n",n, result);
 //
 //	return 0;
