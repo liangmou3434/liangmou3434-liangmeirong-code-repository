0a9e359ad43d7b9dce3dcfaf048fa32d44c42139
liangmou3434 <2685463636@qq.com>
2024-03-06 17:00:15 +0800

    test

--- 2024.1.5加法函数调用代码/test.c
+++ 2024.1.5加法函数调用代码/test.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-//int Add(int x, int y)
+//int Add(int x, int y)///加法代码
 //{
 //	int z = 0;
 //	z = x + y;
@@ -12,7 +12,7 @@
 //	int b = 0;
 //	int ret;
 //	scanf("%d%d", &a, &b);
-//	ret = Add(a, b);
+//	ret = Add(a, b);//调用加法代码
 //	printf("%d ",ret);
 //	return 0;
 //}
