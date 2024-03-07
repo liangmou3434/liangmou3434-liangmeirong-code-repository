b98f8480ed49049cfd3e6900895b6851b5167d1f
liangmou3434 <2685463636@qq.com>
2024-03-07 08:04:56 +0800

    test

--- 2024.1.1猜数字游戏/test.c
+++ 2024.1.1猜数字游戏/test.c
@@ -10,7 +10,7 @@
 //	srand((unsigned int)time(NULL));//调用函数生成不同的随机数
 //	//time函数可以返回一个时间戳，时间戳-北京时间与1970年1月1日0时0分0秒的时间差值，单位为秒 返回类型为time_t类型
 //	//生成0~100的随机数//time_t本质上其实就是32或者64位数的整形类型
-//	number = rand() % 100 + 1;
+//	number = rand() % 100 + 1;//数字范围为0~100
 //	while (scanf("%d",&Figure)!=EOF)
 //	{
 //		if (Figure > number)
