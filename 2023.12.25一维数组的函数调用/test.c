451b1fa473943f3717a7d3da8bbee4fe64f91b60
liangmou3434 <2685463636@qq.com>
2024-03-07 08:18:38 +0800

    test

--- 2023.12.25一维数组的函数调用/test.c
+++ 2023.12.25一维数组的函数调用/test.c
@@ -1,19 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-////void set_arr(int arr[], int sz)
-////{
-////	int i = 0;
-////	for (i = 0; i <= sz; i++)
-////	{
-////		scanf("%d", &arr[i]);
-////		printf("arr[%d]=%d\n", i, arr[i]);
-////	}
-////}
-////int main()
-////{
-////	int arr[10] = { 0 };
-////	int sz = sizeof(arr) / sizeof(arr[0]);
-////	set_arr(arr, sz);
-////	return 0;
-////}
+//void set_arr(int arr[], int sz)//创建一维数组
+//{
+//	int i = 0;
+//	for (i = 0; i <= sz; i++)
+//	{
+//		scanf("%d", &arr[i]);//输入一维数组
+//		printf("arr[%d]=%d\n", i, arr[i]);
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	set_arr(arr, sz);//一维数组的调用
+//	return 0;
+//}
