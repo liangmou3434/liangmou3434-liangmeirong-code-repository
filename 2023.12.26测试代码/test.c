0b763b3150352c919c99e3af5e249d035c252f1e
liangmou3434 <2685463636@qq.com>
2024-03-07 08:16:45 +0800

    输入时带符号，输出时删除符号写法

--- 2023.12.26测试代码/test.c
+++ 2023.12.26测试代码/test.c
@@ -1,10 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-//int main()
-//{
-//	int a, b, c;
-//	scanf("%d%*c%d%*c%d%*c", &a, &b, &c);
-//	printf("%d %d %d\n", a, b, c);
-//	return 0;
-//}
+int main()
+{
+	int a, b, c;
+	scanf("%d%*c%d%*c%d%*c", &a, &b, &c);//输入数字时可携带符号 为打印时删除符号写法
+	printf("%d %d %d\n", a, b, c);
+	return 0;
+}
