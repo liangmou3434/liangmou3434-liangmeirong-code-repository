c224c1ad73425eecd5a8e6d1cefc68dbdcbe20a9
liangmou3434 <2685463636@qq.com>
2024-03-15 22:04:15 +0800

    test

--- 2023.12.7test/test.c
+++ 2023.12.7test/test.c
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-//int main()
+//int main()//比较大小
 //{
 //    int a = 0;
 //    int b = 0;
 //    scanf("%d%d", &a, &b);
-//    if (a > b)
+//    if (a > b)//判断a是否>b
 //    {
 //        printf("a>b");
 //    }
@@ -17,7 +17,7 @@
 //    return 0;
 //}
 
-//int main()
+//int main()//判断大小且打印具体结果
 //{
 //    int a = 0;
 //    int b = 0;
@@ -44,16 +44,16 @@
 //    int t = 0;
 //    while (scanf("%d", &t) != EOF)
 //    {
-//        if (t > 0)
+//        if (t > 0)//判断一个数t 若t大于0 则打印1
 //        {
 //            printf("1\n");
 //        }
-//        else if (t == 0)
+//        else if (t == 0)//若t=0 则打印0.5
 //        {
 //            float a = 1 / 2.0;
 //            printf("%.1f\n", a);
 //        }
-//        else if (t < 0)
+//        else if (t < 0)//若t小于0，则打印0
 //        {
 //            printf("0\n");
 //        }
@@ -61,7 +61,7 @@
 //    return 0;
 //}
 
-//int main()
+//int main()//判断一个数是否是5的倍数 如果是打印yes 不是打印no
 //{
 //    int M = 0;
 //    scanf("%d", &M);
@@ -75,7 +75,7 @@
 //    return 0;
 //}
 
-//int main()
+//int main()//打印星号
 //{
 //    int length = 0;
 //    int n = 0;
