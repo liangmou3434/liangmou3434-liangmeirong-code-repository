53d59fde7a36904f897d8e3fc0379cfc952596cf
liangmou3434 <2685463636@qq.com>
2024-09-10 12:37:13 +0800

    test

---
+++ 2023.12.6算术操作符2.0/2023.1.7输出学生的成绩/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	float grade1, grade2, grade3;
+//	double sum1,sum2,sum3 ;
+//	printf("输入第一个学生的三门课成绩<用空格分隔>:\n");
+//	scanf("%f %f %f", &grade1, &grade2, &grade3);
+//	getchar();
+//	sum1 = grade1 + grade2 + grade3;
+//	printf("%f\n", sum1);
+//	printf("输入第二个学生的三门课成绩<用逗号分隔>:\n");
+//	scanf("%f,%f,%f", &grade1, &grade2, &grade3);
+//	getchar();
+//	sum2 = grade1 + grade2 + grade3;
+//	printf("%f\n", sum2);
+//	printf("输入第三个学生的三门课成绩<用冒号分隔>:\n");
+//	scanf("%f：%f：%f", &grade1, &grade2, &grade3);
+//	getchar();
+//	sum3 = grade1 + grade2 + grade3;
+//	printf("%f\n", sum3);
+//	return 0;
+//}
